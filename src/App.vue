c605e3a8227a26f00b6ab743903c99363fc1b411
felipebrasil8 <felipebrga@hotmail.com>
2019-04-04 03:07:03 +0000

    Continuação...

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,89 @@
 <template>
 
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
 
-    <img v-for="foto in fotos" :src="foto.url" :alt="foto.titulo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+	<input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">   
+
+
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
+    </ul>
 
   </div>
 
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
 
 export default {
+ components: {
 
+    'meu-painel': Painel
+  },
   data () {
     return {
-
-      fotos: []
+	titulo: 'Titulo',
+      fotos: [],
+	ipServer: '142.93.204.36',
+        filtro: '',
 
     }
   },
   created() {
 
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http.get('http://'+this.ipServer+':3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
 
+  },
+computed: {
+
+    fotosComFiltro() {
+
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+
+    }
   }
 }
 </script>
 <style>
+.centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+ .imagem-responsiva{
+	width: 100%;
+}
+.filtro {
+    display: block;
+    width: 100%;
+  }
 </style>
